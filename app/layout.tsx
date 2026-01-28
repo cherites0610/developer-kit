3d36044033824537cbe3fe4340359c7eae7e21ab
cherites <cherites0610@gmail.com>
2026-01-28 14:52:41 +0800

    feat:新增GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/sonner"
+import { GoogleAnalytics } from '@next/third-parties/google'
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
@@ -76,6 +77,7 @@ export default function RootLayout({
         {children}
         <Toaster />
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ""} />
     </html>
   );
 }
